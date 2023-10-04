152af1f9fca330689075df107bacd31e4ec1a9f7
Raphael Ceolato <rceolato@gmail.com>
2023-10-04 14:02:26 -0300

    main : ID 202310041359 - Upload front-end inicial;

---
+++ smed/smed.js
@@ -0,0 +1,8 @@
+$('#demoEvoCalendar').evoCalendar({
+        format: 'dd/MM/yyyy',
+        titleFormat: 'MM',
+        eventHeaderFormat: 'dd / MM / yyyy',
+        theme: 'Midnight Blue',
+        language: 'pt',
+        todayHighlight: true
+});
